3d8b565016832d03a2a4df912fb041ba1eb9a0cf
David Ahn <dja1990@gmail.com>
2019-04-07 21:37:36 -0700

    multiple ternary operator

--- ternary_operator.js
+++ ternary_operator.js
@@ -30,3 +30,36 @@ function checkEqual(a, b) {
   }
   
 checkEqual(1, 2);
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Basic JavaScript: Use Multiple Conditional (Ternary) Operators
+// In the previous challenge, you used a single conditional operator. You can also chain them together to 
+// check for multiple conditions.
+
+// The following function uses if, else if, and else statements to check multiple conditions:
+
+// function findGreaterOrEqual(a, b) {
+//   if(a === b) {
+//     return "a and b are equal";
+//   }
+//   else if(a > b) {
+//     return "a is greater";
+//   }
+//   else {
+//     return "b is greater";
+//   }
+// }
+// The above function can be re-written using multiple conditional operators:
+
+// function findGreaterOrEqual(a, b) {
+//   return (a === b) ? "a and b are equal" : (a > b) ? "a is greater" : "b is greater";
+// }
+
+// Use multiple conditional operators in the checkSign function to check if a number is positive, negative or zero.
+
+function checkSign(num) {
+    return (!num) ? "zero" : (num > 0) ? "positive" : "negative";
+  }
+  
+  checkSign(10);
